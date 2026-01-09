2fdb8c4176b89258ea36e6338cbd137bbb156ad5
Tech Rifki Consultant Network Engineering <rifki.work74@outlook.com>
2026-01-09 18:44:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -186,7 +186,7 @@ YTDL_OPTIONS = {
     'restrictfilenames': True,
     'noplaylist': True,
     'nocheckcertificate': True,
-    'ignoreerrors': False,
+    'ignoreerrors': True,
     'logtostderr': False,
     'quiet': True,
     'no_warnings': True,
