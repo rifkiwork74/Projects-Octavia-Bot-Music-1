b3c008ed6bb4d45c20ce483e08109ce52d71f4ef
rifkiwork74 <rifki.work74@outlook.com>
2026-01-15 21:22:52 +0700

    update main.py

--- main.py
+++ main.py
@@ -205,6 +205,9 @@ YTDL_OPTIONS = {
     # Buffer diperbesar ke 10MB untuk menampung lonjakan data tanpa putus
     'http_chunk_size': 10485760, 
     'expected_protocol': 'https'
+    'headers': {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+    }
 }
 
 
